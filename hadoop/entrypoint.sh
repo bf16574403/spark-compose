a559cf3202ffec712e269460101c34db24d3c193
lcm3 <root@lcm3.leveragedcm.com.au>
2017-02-05 11:05:59 +0000

    Add new property

--- hadoop/entrypoint.sh
+++ hadoop/entrypoint.sh
@@ -50,6 +50,7 @@ if [ "$MULTIHOMED_NETWORK" = "1" ]; then
     addProperty /etc/hadoop/hdfs-site.xml dfs.namenode.https-bind-host 0.0.0.0
     addProperty /etc/hadoop/hdfs-site.xml dfs.client.use.datanode.hostname true
     addProperty /etc/hadoop/hdfs-site.xml dfs.datanode.use.datanode.hostname true
+    addProperty /etc/hadoop/hdfs-site.xml dfs.namenode.datanode.registration.ip-hostname-check false
 
     # YARN
     addProperty /etc/hadoop/yarn-site.xml yarn.resourcemanager.bind-host 0.0.0.0
