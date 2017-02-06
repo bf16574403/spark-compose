25db840da339f9ebb4c96aa00c38d57b8b358038
root <root@lcm4>
2017-02-06 15:41:51 +0000

    Update rancher compose file

---
+++ push-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+namespace="$1"
+
+for i in hadoop hadoop-namenode hadoop-datanode hadoop-resourcemanager hadoop-nodemanager hadoop-historyserver spark spark-master spark-worker; do
+    echo Pushing to docker.io $i
+    ( cd $i && docker build -t $namespace/$i . )
+    docker push $namespace/$i
+done
