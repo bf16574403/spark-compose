eea6197262b98ad2ac5266229209e59b3d6cb1df
bf16574403 <root@bf16574403.com>
2017-03-16 12:50:46 +0100

    Minor fixes

--- push-images.sh
+++ push-images.sh
@@ -2,7 +2,7 @@
 
 namespace="$1"
 
-for i in hadoop hadoop-namenode hadoop-datanode hadoop-resourcemanager hadoop-nodemanager hadoop-historyserver spark spark-master spark-worker zookeeper kafka kafka-manager; do
+for i in hadoop hadoop-namenode hadoop-datanode hadoop-resourcemanager hadoop-nodemanager hadoop-historyserver spark spark-master spark-worker java zookeeper kafka kafka-manager; do
     echo Pushing to docker.io $i
     ( cd $i && docker build -t $namespace/$i . )
     docker push $namespace/$i
