562fe6b1081b0c08ec2e89f2ffcc1e0795719ea7
bf16574403 <root@bf16574403.com>
2017-03-16 12:43:32 +0100

    Merge fix

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-for i in hadoop hadoop-namenode hadoop-datanode hadoop-resourcemanager hadoop-nodemanager hadoop-historyserver spark spark-master spark-worker; do
-    echo Building $i
-    ( cd $i && docker build -t test/$i .)
-=======
 for i in hadoop hadoop-namenode hadoop-datanode hadoop-resourcemanager hadoop-nodemanager hadoop-historyserver spark spark-master spark-worker java zookeeper kafka kafka-manager; do
     echo Building $i
     ( cd $i && docker build -t bf16574403/$i .)
->>>>>>> Add kafka stack
 done
