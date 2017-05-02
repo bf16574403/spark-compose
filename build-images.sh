d1e13cf821b98481cdb3eb97bc9db67bbc573f58
bf16574403 <root@bf16574403.com>
2017-05-02 17:41:52 +0200

    Fix missing logs issue.

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in hadoop hadoop-namenode hadoop-datanode hadoop-resourcemanager hadoop-nodemanager hadoop-historyserver spark spark-master spark-worker java zookeeper kafka kafka-manager elasticsearch logstash kibana; do
+for i in hadoop hadoop-namenode hadoop-datanode hadoop-resourcemanager hadoop-nodemanager hadoop-historyserver spark spark-master spark-worker java zookeeper kafka kafka-manager elasticsearch logstash kibana filebeat; do
     echo Building $i
     ( cd $i && docker build -t bf16574403/$i .)
 done
